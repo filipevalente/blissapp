43549661978559274f9456453d12b7e9ce47adcc
Filipe <filipe@techbiosis.com>
2017-08-01 16:53:53 +0100

    FREQ 02

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+import VueProgressBar from 'vue-progressbar';
 import App from './App';
 import router from './router';
+
+const options = {
+  color: '#4C4C4C',
+  failedColor: '#874b4b',
+  thickness: '2px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300,
+  },
+  autoRevert: true,
+  location: 'top',
+  inverse: false,
+};
+
+Vue.use(VueProgressBar, options);
 
 Vue.use(VueResource);
 
